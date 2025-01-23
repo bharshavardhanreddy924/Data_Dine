6c39eeac050a3aceee5e251b24d81b339800f73a
Harshavardhan Reddy <151720877+bharshavardhanreddy924@users.noreply.github.com>
2025-01-23 16:01:48 +0530

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1773,6 +1773,5 @@ def internal_error(error):
 
 # Initialize the database when the app starts
 init_db()
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
