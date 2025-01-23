28ead980444ca0922ec7542a1f940ff466972035
Harshavardhan Reddy <151720877+bharshavardhanreddy924@users.noreply.github.com>
2025-01-23 20:40:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,11 @@ from bson import ObjectId
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'your-secret-key-change-this')
-
+import certifi
 # MongoDB Configuration
 from pymongo.mongo_client import MongoClient
-
-uri = "mongodb+srv://bharshavardhanreddy924:uLCmWytTkthYz3xJ@data-dine.5oghq.mongodb.net/?retryWrites=true&w=majority&ssl=true&tlsAllowInvalidCertificates=true"
-client = MongoClient(uri)
+uri = "mongodb+srv://bharshavardhanreddy924:uLCmWytTkthYz3xJ@data-dine.5oghq.mongodb.net/?retryWrites=true&w=majority&ssl=true"
+client = MongoClient(uri, tlsCAFile=certifi.where())
 
 # Send a ping to confirm a successful connection
 try:
