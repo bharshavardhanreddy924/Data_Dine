273d5e6f9be4082c838c8e765a440d0978f3ada0
Harshavardhan Reddy <151720877+bharshavardhanreddy924@users.noreply.github.com>
2025-01-23 15:33:45 +0530

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -13,8 +13,20 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'your-secret-key-change-this')
 
 # MongoDB Configuration
-MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/')
-client = MongoClient(MONGO_URI)
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://bharshavardhanreddy924:uLCmWytTkthYz3xJ@data-dine.5oghq.mongodb.net/?retryWrites=true&w=majority&appName=Data-Dine"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 db = client['resta_db']
 
 # File Upload Configuration
