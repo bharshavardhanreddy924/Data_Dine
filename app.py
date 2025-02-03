481dd4e6b42f257f43f71ef123093a3c8e1f7b18
Harshavardhan Reddy <151720877+bharshavardhanreddy924@users.noreply.github.com>
2025-02-03 23:56:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2182,5 +2182,6 @@ def internal_error(error):
 # Initialize the database when the app starts
 init_db()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
