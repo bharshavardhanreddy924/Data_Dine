3deefd9f61ae0677ef8141898054ef45a4a2d8b3
Harshavardhan Reddy <151720877+bharshavardhanreddy924@users.noreply.github.com>
2025-03-17 19:20:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Exit on error
+# Exit on failure
 set -e
 
 echo "Updating system packages..."
@@ -9,11 +9,8 @@ apt-get update && apt-get install -y build-essential libffi-dev libssl-dev libev
 echo "Upgrading pip, setuptools, and wheel..."
 pip install --upgrade pip setuptools wheel
 
-echo "Installing Cython to avoid gevent errors..."
-pip install --no-cache-dir Cython
-
 echo "Installing dependencies..."
 pip install --no-cache-dir -r requirements.txt
 
-echo "Starting Flask app..."
+echo "Starting Flask with Gunicorn..."
 gunicorn -w 4 -b 0.0.0.0:8080 wsgi:app
